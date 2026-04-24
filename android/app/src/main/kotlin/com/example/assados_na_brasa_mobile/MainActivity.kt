a98d5c8cc32af52dc3e666e7ab744957a34a1f15
goemon009 <leucampos@gmail.com>
2026-04-23 21:06:56 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/assados_na_brasa_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.assados_na_brasa_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/config/supabase_config.dart
@@ -0,0 +1,4 @@
+class SupabaseConfig {
+  static const String url = 'https://pizeiusinvyzqurvprom.supabase.co';
+  static const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpemVpdXNpbnZ5enF1cnZwcm9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5NzY2NTgsImV4cCI6MjA5MjU1MjY1OH0.Q_i38-XSb5TYXvddW-ZxZx7P6aDi2kVzBs9j27rZaPo';
+}
